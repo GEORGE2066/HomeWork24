3d3a7ffab39e1a55b3655990674b42dc7764b586
Администратор <george2066@yandex.ru>
2022-09-22 00:08:55 +0300

    Четвёртое домашнее задание второго курса

---
+++ src/main/java/com/example/homework24/service/CalculationService.java
@@ -0,0 +1,8 @@
+package com.example.homework24.service;
+
+public interface CalculationService {
+    public int plus(Integer num1, Integer num2);
+    public int minus(Integer num1, Integer num2);
+    public int multiply(Integer num1, Integer num2);
+    public double divide(Integer num1, Integer num2);
+}
